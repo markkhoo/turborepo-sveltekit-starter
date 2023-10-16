f354ed68cbb4a97b15f90f40368b2b5901b39b5c
Mark Khoo <markdkhoo@gmail.com>
2023-10-15 20:51:18 -0700

    TEST dotenv in web app

---
+++ apps/web/src/routes/+page.server.ts
@@ -0,0 +1,22 @@
+import { env } from '$env/dynamic/private';
+
+export async function load({ params }) {
+  async function safeFetch(url: string, options: RequestInit = {}) {
+    if (!url || !url.startsWith('http')) return Promise.resolve(null);
+    return fetch(url, options).then((res) => {
+      if (res.ok) return res.json();
+      throw new Error(res.statusText);
+    }).catch((err) => {
+      console.error("Fetch Failed: ", err);
+      return null;
+    })
+  }
+
+  return {
+    status: 200,
+    body: {
+      message: `Hello ${params}!`,
+    },
+    data: await safeFetch(env.SERVER_URL)
+  };
+}
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
   import { MyCounterButton } from 'ui';
+
+  export let data;
 </script>
 
 <h1>Web</h1>
 <MyCounterButton />
+
+{#if data && data.data}
+  {#each Object.entries(data.data) as [key, value], index (index)}
+    <p>{key}: {value}</p>
+  {/each}
+{/if}
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
